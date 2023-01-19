553dc7c3b5a681e3000af21ff3cfda182f9c6c46
Cristhian-Chavez <crsth.1990@fpuna.edu.py>
2023-01-18 21:40:03 -0300

    refactor and add state

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            list: ['Sacar la ropa', 'Hacer la cama', 'Leer un rato']
+        }
+    }
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+              {
+                  this.state.list.map( (el) => {
+                      return <li key={el}>{el}</li>
+                  })
+              }
           </ul>
            <form>
              <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
